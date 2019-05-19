77d88c10fd29d84e703524befd865a21927e7684
GabrielJosephGuadiz <dizzygee.0013@gmail.com>
2019-05-19 11:56:11 -0700

    New commit from master branch for personal test (to actually see how long it takes for github to have the info updated on website

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // Problem 1: (this is your demo that we'll solve in class)
-
-
-// Gabriel - Code Below:
+// Code Below:
 
 function sum(a, b) { //eslint-disable-line
-  var result = []; // Gabriel - This Variable returns an empty array 
+  var result = []; 
   result[0] = a + b; 
   result[1] = `The sum of ${a} and ${b} is ${result[0]}.`; // Gabriel - golden nugget :)
   return result;
@@ -14,44 +12,42 @@ function sum(a, b) { //eslint-disable-line
 
 testSum(4, 7);
 
-
-
 // Problem 2:
-
-// Write code here
+// Code below:
 
 function multiply(a, b) { //eslint-disable-line
   var answerMultiply = [];
   answerMultiply[0] = a * b;
-  answerMultiply[1] = `Thre product of ${a} and ${b} is ${answerMultiply}.`;
+  answerMultiply[1] = `The product of ${a} and ${b} is ${answerMultiply}.`;
   return resultMultiply
 }
 
 testMultiply(5,9);
 
-
 // Problem 3:
-
-// Write code here  
+// Code below:
 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var answerSumAndMultiply = []/
+  var answerSumAndMultiply = [];
   var addIt = sum(a, b)[0];
   var timesIt = multiply(a, b)[0];
   answerSumAndMultiply[0] = sum(addIt, c)[0];
   answerSumAndMultiply[1] = multiply(timesIt, c)[0];
   answerSumAndMultiply[2] = `${a} and ${b} and ${c} and ${answerSumAndMultiply[0]}.`;
   answerSumAndMultiply[3] = `The product of ${a} and ${b} and ${c} is ${answerSumAndMultiply[1]}.`;
-  return resultSumAndMultiply
+  return answerSumAndMultiply
 }
 
 testSumAndMultiply(4,7,5);
 
 // Problem 4:
+// Code below:
 
-// Write your code here
 
 var testArray = [2, 3, 4]; //eslint-disable-line
+// Gabriel - Above looks to be setting a global variable for the function down below.
+// Gabriel - What does the 'disable' lint do? And how does it effect or if it effects workflow or workspeed, (if there are limiters on what we are doing?)
+
 
 function sumArray(sumArr) { //eslint-disable-line
   var answerSumArray = [];
@@ -62,15 +58,12 @@ function sumArray(sumArr) { //eslint-disable-line
 
 testSumArray(testArray);
 
-
-
 // Problem 5:
-
 // Write your code here
 
 function multiplyArray(multArr) { //eslint-disable-line
   var answerMultiplyArray = [];
-  answerMultiplyArray[0] = multiply(multiply(multArr[0], mutlArr[1])[0], multArr[2])[0];
+  answerMultiplyArray[0] = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
   answerMultiplyArray[1] = `the numbers ${multArr[0]}, ${multArr[1]}, ${multArr[2]} have a product of ${answerMultiplyArray}.`;
   return answerMultiplyArray;
 }
